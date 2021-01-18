d1ac7529864a6adefcf25163afe924faec979ee3
King <42466804+KingSHcoder@users.noreply.github.com>
2021-01-18 14:49:30 +0800

    Initial Commit

---
+++ Shared/LegolasDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegolasDemoApp.swift
+//  Shared
+//
+//  Created by King Genius on 2021/1/18.
+//
+
+import SwiftUI
+
+@main
+struct LegolasDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
